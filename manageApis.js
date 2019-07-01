894272ec35ad5a0d0c633070f1a21bc18b27aca4
Kevin Rodríguez <40246694+Kev12997@users.noreply.github.com>
2019-07-01 16:17:39 -0400

    Update manageApis.js

--- manageApis.js
+++ manageApis.js
@@ -36,7 +36,7 @@
       animation-duration: 1s
     }
     
-    @-webkit-keyframes animatebottom {
+    @-webkit-keyframes animatebottom { 
       from { bottom:-100px; opacity:0 } 
       to { bottom:0px; opacity:1 }
     }
